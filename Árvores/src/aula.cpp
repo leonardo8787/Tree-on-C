bc0082f86dc4a3024510f161d1a41c6cfc2ea5f1
leonardo8787 <leocampos8787@gmail.com>
2021-10-22 19:13:41 -0300

    atualização

---
+++ Árvores/src/aula.cpp
@@ -0,0 +1,43 @@
+#include "aula.h"
+
+Tree* CreateTree(){
+	return NULL;
+}
+
+void insertItem(Tree **t, Record r){
+
+	if(*t == NULL){
+
+		*t = (Tree*)malloc(sizeof(Tree)); // record + filho esquerdo + filho direito
+		(*t)->esq = NULL;
+		(*t)->dir = NULL;
+		(*t)->reg = r;
+
+	} else {
+
+		if(r.value < (*t)->reg.value)
+			insertItem(&(*t)->esq, r);
+
+		else if(r.value > (*t)->reg.value)
+			insertItem(&(*t)->dir, r);
+
+	}
+}
+
+int main(){
+
+	Tree *t;
+	Record r;
+
+	t = CreateTree();
+
+	r.value = 10;
+	insertItem(&t, r);
+
+	r.value = 8;
+	insertItem(&t, r);
+
+	r.value = 10;
+	insertItem(&t, r);
+	
+}
---
+++ Árvores/src/aula.h
@@ -0,0 +1,43 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+Árvore básica: árvore binária
+	-> tem um nó pai -> 2 filhos (direito >, esquerdo <)
+
+	Input: {10,8,5,9,12,17}  log_n = log6 # 2.14
+
+		  10 <- root
+		/    \
+	   NULL  NULL  <- *t
+
+		 10  <- root (nível 0) 
+		/  \
+	   8    12  <- nível 1
+	  / \    \
+	 5   9    17  <- nível 2 (folhas)
+
+O pior caso para usar a árvore é quando os dados já estão ordenados
+
+*/
+
+struct Record{
+	int value;
+};
+
+struct heap{
+	Record reg;
+	Tree *esq, *dir;
+};
+
+Tree* createTree();
+void insertItem(Tree **t, Record r);
+void pesquisa(Tree **t, Tree **aux, Record  r);
+
+
+
+#endif
